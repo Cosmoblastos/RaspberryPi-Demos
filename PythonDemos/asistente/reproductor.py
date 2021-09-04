8018a91dadcb8b667d3bb314c717de8c73d174ed
Alejandro Zapett <alejandro.zapett@gmail.com>
2021-09-04 01:49:56 -0500

    prueba de bocinas

--- PythonDemos/asistente/reproductor.py
+++ PythonDemos/asistente/reproductor.py
@@ -2,3 +2,22 @@ import os
 import subprocess
 import random
 from os.path import join, realpath
+
+_random = random
+_subprocess = subprocess
+detect_in_row = 0
+
+folder = realpath(join(realpath(__file__), 'fun', 'arnold_audio'))
+arnold_mp3 = [
+    os.path.join(folder, x)
+    for x in os.listdir(folder)
+    if ".wav" in x
+]
+
+filename = _random.choice(arnold_mp3)
+try:
+    print("playing", filename)
+    _subprocess.check_output(['play', '-v', '.1', filename])
+except Exception as e:
+    print(str(e))
+        
